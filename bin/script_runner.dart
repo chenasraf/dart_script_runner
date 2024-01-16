00c5ae43140b1d9f95bf61d41384362a3a7e6c82
Chen Asraf <casraf@pm.me>
2024-01-16 02:54:43 +0200

    fix: config factory
    chore: remove yaml dependency

--- bin/script_runner.dart
+++ bin/script_runner.dart
@@ -3,11 +3,15 @@ import 'package:script_runner/src/utils.dart';
 
 /// Main entrypoint for CMD script runner.
 Future<void> main(List<String> args) async {
+  if (args.isEmpty) {
+    printColor('No script command provided. Use -h to see available commands.', [TerminalColor.red]);
+    return;
+  }
   final scriptCmd = args.first;
   final scriptArgs = args.sublist(1);
   try {
     await runScript(scriptCmd, scriptArgs);
-  } catch (e) {
-    printColor('$e', [TerminalColor.red]);
+  } catch (e, stack) {
+    printColor('$e\n$stack', [TerminalColor.red]);
   }
 }
